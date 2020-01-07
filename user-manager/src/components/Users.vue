1f433c78a4c103f5d699790caa5d84c6fb6edae7
Logan Shoemaker <logansshoemaker@gmail.com>
2020-01-07 09:31:47 -0700

    added users array and mapping out prop

---
+++ user-manager/src/components/Users.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>These are your users</h1>
+        <h3 v-for="(user, index) in users" v-bind:key="index">
+            {{ user }}
+        </h3>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "Users",
+    props: {
+        users: {
+            type: Array,
+            default: () => ["Ash", "Brittany", "Jack", "Logan"]
+        }
+    }
+});
+</script>
